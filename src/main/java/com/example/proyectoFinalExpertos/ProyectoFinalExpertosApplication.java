b980c62078c20e3e41fb80cfcd19f61bf259e224
zirpin <zirpin@gmail.com>
2021-04-13 17:15:41 +0200

    initial commit

---
+++ src/main/java/com/example/proyectoFinalExpertos/ProyectoFinalExpertosApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectoFinalExpertos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalExpertosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalExpertosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/proyectoFinalExpertos/ProyectoFinalExpertosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectoFinalExpertos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalExpertosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
