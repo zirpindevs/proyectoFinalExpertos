619466cd3a016659595d79f730f20c27e834461f
zirpin <zirpin@gmail.com>
2021-04-18 17:39:25 +0200

    creacion de mas datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,19 +1,52 @@
-insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad) values ('name1', 'surname 1', '10000A',"master1", 30, "pendiente", "inmediata");
-insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad) values ('name2', 'surname 2', '20000A',"master2", 40, "validado", "media");
-insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad) values ('name3', 'surname 3', '30000A',"master3", 50, "pendiente", "inmediata");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('juan', 'lopez', '10000A',"curso1", 30, "pendiente", "inmediata", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('rafa', 'montero', '20000A',"curso2", 40, "validado", "media", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('javi', 'diaz', '30000A',"curso3", 50, "pendiente", "inmediata", "");
+
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('juan', 'gomez', '40000A',"curso4", 30, "pendiente", "inmediata", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('alan', 'palomo', '50000A',"curso5", 40, "validado", "media", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('miguel', 'gavilan', '60000A',"curso6", 20, "pendiente", "total", "");
+
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('jesus', 'rojas', '70000A',"curso7", 30, "pendiente", "inmediata", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('antonio', 'cabezas', '80000A',"curso8", 40, "descartado", "total", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('marco', 'leal', '30000A',"curso9", 50, "pendiente", "inmediata", "");
+
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('rafa', 'gonzalez', '90000A',"curso10", 30, "pendiente", "parcial", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('joaquin', 'espejo', '100000B',"curso11", 40, "validado", "media", "");
+insert into experts (name, surname, nif, cursos, condiciones, estado, disponibilidad, modalidad) values ('julio', 'gonzalez', '110000B',"curso12", 150, "pendiente", "inmediata", "");
 
 
-insert into users (name, surname, dni, is_active) values ('name1', 'surname 1', '10000A', true);
-
-insert into tags(name) values ('tag1');
-insert into tags(name) values ('tag2');
-insert into tags(name) values ('etiqueta1');
-insert into tags(name) values ('etiqueta2');
-insert into tags(name) values ('limpieza');
+insert into tags(name) values ('php');
+insert into tags(name) values ('java');
+insert into tags(name) values ('python');
+insert into tags(name) values ('angular');
+insert into tags(name) values ('linux');
 
 
 insert into pivot (expert_id, tag_id) values (1, 1);
 insert into pivot (expert_id, tag_id) values (1, 2);
+
 insert into pivot (expert_id, tag_id) values (2, 3);
 
+insert into pivot (expert_id, tag_id) values (3, 3);
+insert into pivot (expert_id, tag_id) values (4, 5);
 
+insert into pivot (expert_id, tag_id) values (5, 3);
+insert into pivot (expert_id, tag_id) values (5, 2);
+
+insert into pivot (expert_id, tag_id) values (6, 4);
+insert into pivot (expert_id, tag_id) values (7, 2);
+
+insert into pivot (expert_id, tag_id) values (8, 4);
+insert into pivot (expert_id, tag_id) values (8, 1);
+
+insert into pivot (expert_id, tag_id) values (9, 5);
+
+insert into pivot (expert_id, tag_id) values (10, 3);
+insert into pivot (expert_id, tag_id) values (10, 1);
+
+
+insert into pivot (expert_id, tag_id) values (11, 2);
+insert into pivot (expert_id, tag_id) values (12, 4);
+
+
+insert into users (name, surname, dni, is_active) values ('name1', 'surname 1', '10000A', true);
