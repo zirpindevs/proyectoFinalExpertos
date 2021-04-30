5b5a05c587b2e71f96f75283661f0cc1b6c7ce56
zirpin <zirpin@gmail.com>
2021-04-30 19:32:50 +0200

    pepe

--- src/main/java/com/example/proyectoFinalExpertos/controller/TagController.java
+++ src/main/java/com/example/proyectoFinalExpertos/controller/TagController.java
@@ -90,7 +90,7 @@ public class TagController {
      * FIND ALL TAGS
      * @return List<Tag>
      */
-    @RequestMapping(method = RequestMethod.GET, value = "/etiquetas")
+    @GetMapping("/etiquetas/pepe")
     public List<Tag> findTags(){
         log.debug("REST request to find all Tags");
 
