531740334b072cf63dc065e7245a6e11a59d0c90
zirpin <zirpin@gmail.com>
2021-04-30 18:23:29 +0200

    @CrossOrigin(origins = "*", allowedHeaders = "*")

--- src/main/java/com/example/proyectoFinalExpertos/controller/TagController.java
+++ src/main/java/com/example/proyectoFinalExpertos/controller/TagController.java
@@ -90,6 +90,7 @@ public class TagController {
      * FIND ALL TAGS
      * @return List<Tag>
      */
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping("/etiquetas")
     public List<Tag> findTags(){
         log.debug("REST request to find all Tags");
