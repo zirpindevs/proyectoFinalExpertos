6b312dc383448acf9a6f5bb37957759efb92c188
zirpin <zirpin@gmail.com>
2021-05-07 14:19:14 +0200

    quitar comentarios y   @CrossOrigin(origins = "*", allowedHeaders = "*")                          @RequestMapping(method = RequestMethod.GET, value = "/expertos")

--- src/main/java/com/example/proyectoFinalExpertos/controller/TagController.java
+++ src/main/java/com/example/proyectoFinalExpertos/controller/TagController.java
@@ -91,7 +91,7 @@ public class TagController {
      * @return List<Tag>
      */
     @CrossOrigin(origins = "*", allowedHeaders = "*")
-    @GetMapping("/etiquetas")
+    @RequestMapping(method = RequestMethod.GET, value = "/etiquetas")
     public List<Tag> findTags(){
         log.debug("REST request to find all Tags");
 
