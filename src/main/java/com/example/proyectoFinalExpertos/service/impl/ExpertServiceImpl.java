b6098b1623bff7a2605583a59c414c8b835fda3a
zirpin <zirpin@gmail.com>
2021-05-09 23:12:00 +0200

    update

--- src/main/java/com/example/proyectoFinalExpertos/service/impl/ExpertServiceImpl.java
+++ src/main/java/com/example/proyectoFinalExpertos/service/impl/ExpertServiceImpl.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -55,50 +56,15 @@ public class ExpertServiceImpl implements ExpertService {
     public Expert updateExpert(Long id, Expert modifiedExpert) {
         log.info("REST request to update an expert");
 
-        Tag findedTag = new Tag();
-        Expert findedExpert = this.findOne(id);
-        List<Tag> existingTags = findedExpert.getTags();
+        Expert result = null;
 
-        if(findedExpert.getTags().size() < modifiedExpert.getTags().size()) {
-            findedTag = tagDAO.findByName(modifiedExpert.getTags().get(modifiedExpert.getTags().size()-1).getName());
+        if (expertRepository.existsById(modifiedExpert.getId())) {
+            modifiedExpert.setLast_updated(Instant.now());
+            result = expertRepository.save(modifiedExpert);
         }
-            return this.expertDAO.updateExpert(modifiedExpert, findedExpert, findedTag, existingTags);
+
+        return result;
     }
-
-
-    /*@Override
-    public Expert updateExpert(Long id, Expert expert) {
-        log.info("REST request to update an expert");
-
-        Expert findExpert = this.findOne(id);
-        Tag getNewTag = new Tag();
-        List<Tag> listExistedTags = findExpert.getTags();
-        Long newTagId = expert.getTags().get(0).getId();
-        String newTagName = expert.getTags().get(0).getName();
-
-        if(findExpert == null) {
-            return null;
-        }
-
-        System.out.println("###########################    SERVICE    ###################################################");
-
-//esto no parece que busque*******************
-        if(listExistedTags.contains(newTagId) == false){
-            System.out.println("resultado de busqueda falso, podemos insertar");
-
-            //buscamos si ya existe
-            getNewTag = tagDAO.findById(newTagId);
-            System.out.println(getNewTag);
-            //si es null no existe, y hay que crear la etiqueta primero
-            if(getNewTag == null){
-                getNewTag = tagDAO.createTag(newTagName);
-            }
-        }
-
-        System.out.println("##############################################################################");
-
-        return this.expertDAO.modifyExpert(expert, findExpert, getNewTag, listExistedTags);
-    }*/
 
     @Override
     public List<Expert> findAll() {
